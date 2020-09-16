6f35b1a48aa91546d35635bd8a257ae97cda2240
Im-Tae <dlaxo2420@naver.com>
2020-09-17 01:37:46 +0900

    「INIT」 setting

---
+++ src/main/kotlin/com/imtae/gsmsoomgoserver/GsmSoomgoServerApplication.kt
@@ -0,0 +1,11 @@
+package com.imtae.gsmsoomgoserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GsmSoomgoServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<GsmSoomgoServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/imtae/gsmsoomgoserver/GsmSoomgoServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.imtae.gsmsoomgoserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GsmSoomgoServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
